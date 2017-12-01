729a8699fb51ec93a30ab54ef677ba32f90c66c5
timred <xynomix@gmail.com>
2017-12-01 15:10:05 +0000

    Use an ArrayList and start adding childViews from Java code.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,17 +16,32 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         // Create an array of numbers
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Find the rootView that will contain the childViews for each number text
+        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        for(int i = 0; i < words.size(); i++) {
+            // Create a new {@link TextView} that displayed the word at each index
+            // and add the View as a child to the rootView
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            rootView.addView(wordView);
+        }
+
+        // Print each element to the logs
+        for(int i = 0; i < words.size(); i++) {
+            Log.v("NumbersActivity","Word at index " + i + ": " + words.get(i));
+        }
 
     }
 }
